051af90693817d01be7f11cee3cf9b58da5a0ca9
Matheus Amaral da Costa <161625293+MatheusADC@users.noreply.github.com>
2026-03-05 09:22:10 -0300

    ♻️refactor: Filter in Home

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,9 +1,11 @@
-import { Image, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import { styles } from "./styles";
 import { Button } from "@/components/Button";
 import { Input } from "@/components/Input";
 import { Filter } from "@/components/Filter";
 import { FilterStatus } from "@/types/FilterStatus";
+
+const FILTER_STATUS: FilterStatus[] = [FilterStatus.PENDING, FilterStatus.DONE];
 
 export function Home() {
   return (
@@ -14,8 +16,14 @@ export function Home() {
         <Button title="Entrar" />
       </View>
       <View style={styles.content}>
-        <Filter status={FilterStatus.DONE} isActive />
-        <Filter status={FilterStatus.PENDING} isActive={false} />
+        <View style={styles.header}>
+          {FILTER_STATUS.map((status) => (
+            <Filter key={status} status={status} isActive />
+          ))}
+          <TouchableOpacity style={styles.clearButton}>
+            <Text style={styles.clearText}>Limpar</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
